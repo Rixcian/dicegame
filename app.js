a8a7e66e0fd967125fd30818f563fdcb42cdd1b6
Rixcian <rostislav.kremecek@gmail.com>
2017-05-05 15:34:53 +0200

    Change of gaming rules
    Change of gaming rules.

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 /*
+
 GAME RULES:
 
 - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
+- The first player to reach 50 points on GLOBAL score wins the game
+
+HERNÍ PRAVIDLA:
+
+- Hra má 2 hráče, hraje se na kola
+- V každém kole hází hráč kostkou kolikrát chce. Každá kombinace, kterou hodí se mu přičte do ROUND skóre
+- ALE pokud hráč hodí 1, celé kolo ztrácí a je na řadě protihráč
+- Hráč může kliknout na 'HOLD', což znamená, že se mu jeho ROUND skóre přičte ke GLOBAL skóre. Po té je na řadě druhý hráč
+- Ten který první získá 50 GLOBAL skóre vyhrává hru
 
 */
 
